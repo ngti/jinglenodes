2b33388a59abbfa040732c6075042f7d1ccf921a
Ahmed Omar <spawn.think@gmail.com>
2015-05-19 12:04:14 +0200

    Use local rebar

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ rm -rf deps/*
 rm -rf apps/*/logs
 rm -rf apps/*/.eunit
 rm -rf apps/*/doc
-rebar clean delete-deps
-rebar get-deps compile
-rebar eunit skip_deps=true
-rebar ct skip_deps=true
-cd rel && rebar generate && cd ..
+./rebar clean delete-deps
+./rebar get-deps compile
+./rebar eunit skip_deps=true
+./rebar ct skip_deps=true
+cd rel && ../rebar generate && cd ..
