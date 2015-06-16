993d19446f096366115a5bf9707acf8643ad9c05
Ahmed Omar <spawn.think@gmail.com>
2015-06-16 10:08:43 +0200

    Keep consistent naming

--- docker/configure_jnode.sh
+++ docker/configure_jnode.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-sed -i.bak s/server.com/$XMPP_DOMAIN/g /opt/jinglenodes/etc/app.config
+sed -i.bak s/server.com/$JN_XMPP_DOMAIN/g /opt/jinglenodes/etc/app.config
 sed -i.bak s/8889/$JN_XMPP_PORT/g /opt/jinglenodes/etc/app.config
 sed -i.bak s/secret/$JN_XMPP_PASS/g /opt/jinglenodes/etc/app.config
 sed -i.bak s/88.88.88.88/$JN_PUBLIC_IP/g /opt/jinglenodes/etc/app.config
