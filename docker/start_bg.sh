a2f7566b866f560d25510f31b3d9f030214fdde1
Stefan Larsson <stefan@ngti.nl>
2015-06-30 14:42:02 +0200

    HC-339 send logs to console, not the config file

--- docker/start_bg.sh
+++ docker/start_bg.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 /opt/jinglenodes/bin/configure_jnode.sh 
-/opt/jinglenodes/bin/jnode start && tail -F /opt/jinglenodes/etc/vm.args
+/opt/jinglenodes/bin/jnode start && tail -F /opt/jinglenodes/logs/erlang.log /opt/jinglenodes/logs/info.log /opt/jinglenodes/logs/run_erl.log /opt/jinglenodes/logs/crash.log
