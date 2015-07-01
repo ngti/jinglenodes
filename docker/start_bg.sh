fa8917cc9a71105686902b15b7166b45ba711007
Stefan Larsson <stefan@ngti.nl>
2015-07-01 10:21:26 +0200

    HC-339 use proper "log" directory, not "logs" for jinglenodes

--- docker/start_bg.sh
+++ docker/start_bg.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 /opt/jinglenodes/bin/configure_jnode.sh 
-/opt/jinglenodes/bin/jnode start && tail -F /opt/jinglenodes/logs/erlang.log /opt/jinglenodes/logs/info.log /opt/jinglenodes/logs/run_erl.log /opt/jinglenodes/logs/crash.log
+/opt/jinglenodes/bin/jnode start && tail -F /opt/jinglenodes/log/erlang.log /opt/jinglenodes/log/info.log /opt/jinglenodes/log/run_erl.log /opt/jinglenodes/logs/crash.log
