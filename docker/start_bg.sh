3f6c22e09efb7aa193b9fdf784979e10652cbe52
Stefan Larsson <stefan@ngti.nl>
2015-07-06 12:07:54 +0200

    HC-339 Tail all the erlang.log.? files, not the non-existing erlang.log.

--- docker/start_bg.sh
+++ docker/start_bg.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 /opt/jinglenodes/bin/configure_jnode.sh 
-/opt/jinglenodes/bin/jnode start && tail -F /opt/jinglenodes/log/erlang.log /opt/jinglenodes/log/info.log /opt/jinglenodes/log/run_erl.log /opt/jinglenodes/logs/crash.log
+/opt/jinglenodes/bin/jnode start && tail -F /opt/jinglenodes/log/erlang.log.1 /opt/jinglenodes/log/erlang.log.2 /opt/jinglenodes/log/erlang.log.3 /opt/jinglenodes/log/erlang.log.4 /opt/jinglenodes/log/erlang.log.5 /opt/jinglenodes/log/info.log /opt/jinglenodes/log/run_erl.log /opt/jinglenodes/logs/crash.log
