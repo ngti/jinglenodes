8b9ef2eb52b15313387515fe94145749933a0eec
Ahmed Omar <spawn.think@gmail.com>
2016-11-02 18:05:56 +0100

    Configure XMPP host

--- docker/configure_jnode.sh
+++ docker/configure_jnode.sh
@@ -3,4 +3,5 @@ sed -i.bak s/server.com/$JN_XMPP_DOMAIN/g /opt/jinglenodes/etc/app.config
 sed -i.bak s/8889/$JN_XMPP_PORT/g /opt/jinglenodes/etc/app.config
 sed -i.bak s/secret/$JN_XMPP_PASS/g /opt/jinglenodes/etc/app.config
 sed -i.bak s/88.88.88.88/$JN_PUBLIC_IP/g /opt/jinglenodes/etc/app.config
+sed -i.bak s/ejabberd.local/$JN_XMPP_HOST/g /opt/jinglenodes/etc/app.config
 
